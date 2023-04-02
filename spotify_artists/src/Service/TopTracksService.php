ab35a9ea8a0faf863fbdabd8d2dd181f92e53710
Shahar Cohen <shaharcoh@gmail.com>
2023-04-02 13:21:11 +0100

    Added top tracks feature.

---
+++ spotify_artists/src/Service/TopTracksService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\spotify_artists\Service;
+
+/**
+ * Service to top tracks.
+ */
+class TopTracksService {
+
+  use SpotifyApiTrait;
+
+  /**
+   * Function to get top tracks for an artist.
+   */
+  public function getTopTracks(string $artistId): array {
+    return $this->request(
+      "/v1/artists/$artistId/top-tracks",
+      ['id' => $artistId, 'market' => 'GB'],
+      'tracks'
+    );
+  }
+
+}
